e342966d303b18ffbc0005c51274a3a023dd3657
Alina Kotsiubynska <alinakotsiubynska@gmail.com>
2021-01-31 00:17:50 +0200

    попытка подключить слайдер

---
+++ src/scripts/slick-script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('.slider').slick();
+});
+
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  appendArrows: $('.review__button')
+});
